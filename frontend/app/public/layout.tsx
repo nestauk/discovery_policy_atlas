9b2fdad9be0d71c42ec971acf1e97781e99bb59c
Shabeer Rauf <shabeer.rauf@nesta.org.uk>
2026-02-18 15:19:19 +0000

    add footer to public urls

---
+++ frontend/app/public/layout.tsx
@@ -0,0 +1,14 @@
+import { Footer } from '@/components/Footer'
+
+export default function PublicLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <main className="flex-1">{children}</main>
+      <Footer />
+    </div>
+  )
+}
